93069951c300038afe435b12413c242d67a26d21
Yonas Woldemichael <ybyonas@gmail.com>
2022-02-03 15:15:44 -0500

    fixed typo in navtabs

--- src/components/NavTabs/Navtabs.js
+++ src/components/NavTabs/Navtabs.js
@@ -7,10 +7,10 @@ function Navtabs({ handlePageChange }) {
             <div className="row text-white bg-black py-1">
                 <h1 className="col aboutM mx-4">Yonas Woldemichael</h1>
                 <div className="col d-flex flex-row-reverse align-items-end">
-                    <a href="https://drive.google.com/file/d/1ljraWxreLJGLUphwxcq5H6Z4vePdqj_1/view?usp=sharing" target="_blank" className="p-3 aboutMe text-decoration-none text-white">Resume</a>
+                    <a href="https://drive.google.com/file/d/1ljraWxreLJGLUphwxcq5H6Z4vePdqj_1/view?usp=sharing" target="_blank" rel="noreferrer" className="p-3 aboutMe text-decoration-none text-white">Resume</a>
                     <a href="#contact" onClick={() => handlePageChange('Contact')} className="p-3 aboutMe text-decoration-none text-white">Contact Me</a>
                     <a href="#projects" onClick={() => handlePageChange('Projects')} className="p-3 aboutMe text-decoration-none text-white">Projects</a>
-                    <a hred="#home" onClick={() => handlePageChange('Home')} className="p-3 aboutMe text-decoration-none text-white">Home</a>
+                    <a href="#home" onClick={() => handlePageChange('Home')} className="p-3 aboutMe text-decoration-none text-white">Home</a>
                 </div>
             </div>
         </div>
